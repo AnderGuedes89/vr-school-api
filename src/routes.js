64edc32a7d1c10684def78aa183035a1ad06b845
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-02-19 13:19:42 -0300

    Update de aluno

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const StudentController = require('./controllers/StudentController');
 const CourseController = require('./controllers/CourseController');
 const routes = express.Router();
 
+const Student = require('../src/models/Student');
+const Course = require('../src/models/Course');
+
 routes.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -18,5 +21,16 @@ routes.post('/students', StudentController.post);
 routes.get('/courses', CourseController.getAll);
 routes.get('/courses/:id', CourseController.getById);
 routes.post('/courses', CourseController.post);
+
+routes.put('/students/:id', function (req, res, next) {
+    Student.update(
+        { name: req.body.name },
+        { returning: true, where: { id: req.params.id } }
+    )
+        .then(function (students) {
+            res.json(students)
+        })
+        .catch(next)
+});
 
 module.exports = routes;
